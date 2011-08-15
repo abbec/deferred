a6f07d8e23579cc595fa3cda08f4ce3100b3fd18
Albert Cervin <albert@nat0.se>
2011-08-15 21:58:39 +0200

    Removed gamma correction (lazy solution) and added F11 as shortcut for toggle fullscreen.

--- Src/Scene/Scene.cpp
+++ Src/Scene/Scene.cpp
@@ -102,7 +102,7 @@ HRESULT Scene::init(ID3D10Device *device, ID3D10Effect *effect)
 	_skybox->set_single_material(sb_material);
 
 	// Set up lighting
-	_lights.push_back(new Deferred::DirectionalLight(D3DXVECTOR4(0.5, 0.5, 0.5, 1.0), 
+	_lights.push_back(new Deferred::DirectionalLight(D3DXVECTOR4(1.0, 0.92, 0.73, 1.0), 
 		D3DXVECTOR3(1.0, 1.0, 5.0), D3DXVECTOR3(0.0, 0.0, 0.0) - D3DXVECTOR3(1.0, 1.0, 5.0)));
 
 	_lights.push_back(new Deferred::PointLight(D3DXVECTOR4(1.0, 1.0, 1.0, 1.0), 
@@ -110,7 +110,6 @@ HRESULT Scene::init(ID3D10Device *device, ID3D10Effect *effect)
 
 	_lights.push_back(new Deferred::PointLight(D3DXVECTOR4(1.0, 1.0, 1.0, 1.0), 
 		D3DXVECTOR3(5.0, 0.0, 0.0)));
-
 
 	// Initialize the world matrix
     D3DXMatrixIdentity( &_world );
